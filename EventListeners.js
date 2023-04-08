22e5b9070aa9b0b0fcbbdf45afda256e27330765
SUDHEER341 <sudheer601220@gmail.com>
2023-04-08 16:47:50 +0530

    Added : Create employee payroll object on save

--- EventListeners.js
+++ EventListeners.js
@@ -23,3 +23,67 @@
             output.innerHTML = salary.value;
             });
     });
+// UC3
+    const save = () => {
+        try
+        {
+            let EmployeePayrollData = createEmployeePayroll();
+
+        }
+        catch (e){
+            return;
+        }
+    }
+
+    const createEmployeePayroll = () =>
+    {
+        let employeePayrollData = new EmployeePayrollData();
+        try
+        {
+            employeePayrollData.name = getInputValueById('#name');
+
+        }
+        catch (e)
+        {
+            setTextValue('.text-error' , e );
+            throw e;
+        }
+    
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]').pop();
+    employeePayrollData.salary = getSelectedValues('#salary');
+    employeePayrollData.note = getSelectedValues('#notes');
+
+    let date = getInputValueById('#day') + " "
+                + getInputValueById ('#month')
+                + " " +getInputValueById('#year');
+
+    employeePayrollData.date = Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+
+    }
+
+    const getSelectedValues = (propertyValue) =>
+    {
+        let allItems = document.querySelectorAll(propertyValue);
+        let selItems = [];
+        allItems.forEach (item =>{
+            if(item.checked)
+            selItems.push(item.value);
+        });
+        return selItems;
+    }
+
+    const getInputValueById = (id) =>
+    {
+        let value = document.querySelector(id).value;
+        return value;
+    }
+
+    const getInputElementValue = (id) =>
+    {
+        let value  = document.getElementById(id).value;
+        return value;
+    }
